ac2283b4daa902450df7968d02a78d9aeaec6323
lingon <48642331+lingonsylt@users.noreply.github.com>
2025-12-01 15:25:44 +0100

    almost done with shoppinglist

--- src/routes/shoppinglist/+page.svelte
+++ src/routes/shoppinglist/+page.svelte
@@ -1,12 +1,112 @@
+<script>
+	import { fade } from 'svelte/transition';
+
+	let varor = $state([
+		{
+			name: 'Exempelvara',
+			köpt: false,
+			priority: 0
+		},
+		{
+			name: 'Köpt exempel',
+			köpt: true,
+			priority: 1
+		}
+	]);
+	let input = $state('');
+	function handleAdd(event) {
+		event.preventDefault();
+		if (input.trim() != '') {
+			varor.push({
+				name: input.trim(),
+				köpt: false
+			});
+		}
+		input = '';
+	}
+	function removeVara(i) {
+		varor.splice(i, 1);
+	}
+	function moveVara(i) {
+		varor[i].köpt = !varor[i].köpt;
+	}
+	function clear(targetState) {
+		varor = varor.filter((vara) => vara.köpt != targetState);
+	}
+</script>
+
 <main class="container">
 	<h1>Shopping List</h1>
-	<div class="categories-container"></div>
+	<div class="categories-container">
+		<section>
+			<h2>Varör att köpa</h2>
+			<button
+				onclick={() => {
+					clear(false);
+				}}
+				class="clear-button">Rensa</button
+			>
+			<ol>
+				{#each varor as vara, i (vara)}
+					{#if !vara.köpt}
+						<li transition:fade>
+							<input class="priority" type="number" />
+							{vara.name}
+							<button
+								onclick={() => {
+									removeVara(i);
+								}}>X</button
+							>
+							<button
+								onclick={() => {
+									moveVara(i);
+								}}>></button
+							>
+						</li>
+					{/if}
+				{/each}
+			</ol>
+			<form class="input" onsubmit={handleAdd}>
+				<label for="name-fielt">Lägg till vara</label>
+				<input id="name-field" type="text" bind:value={input} />
+				<button type="submit">+</button>
+			</form>
+		</section>
+		<section>
+			<h2>Köpta varor</h2>
+			<button
+				onclick={() => {
+					clear(true);
+				}}
+				class="clear-button">Rensa</button
+			>
+			<ul>
+				{#each varor as vara, i (vara)}
+					{#if vara.köpt}
+						<li transition:fade>
+							{vara.name}
+							<button
+								onclick={() => {
+									removeVara(i);
+								}}>X</button
+							>
+							<button
+								onclick={() => {
+									moveVara(i);
+								}}>{'<'}</button
+							>
+						</li>
+					{/if}
+				{/each}
+			</ul>
+		</section>
+	</div>
 </main>
 
 <style>
 	.container {
 		display: grid;
-		grid-template-rows: 1fr 8fr 1fr;
+		grid-template-rows: 1fr 9fr 1fr;
 		margin: auto;
 		background-color: darkred;
 		width: 60vw;
@@ -15,15 +115,57 @@
 	}
 	.container h1 {
 		justify-self: center;
-		align-self: center;
 		background-color: red;
 		border-radius: 10px;
 		padding: 10px;
 	}
 	.categories-container {
 		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		grid-column-gap: 10px;
 		height: 100%;
 		background-color: red;
-		border-radius: 10px;
+	}
+	.categories-container section {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+	.categories-container section:nth-child(odd) {
+		background-color: rgba(0, 0, 0, 0.1);
+	}
+	.categories-container section:nth-child(even) {
+		background-color: rgba(0, 0, 0, 0.3);
+	}
+	section h2 {
+		background-color: rgba(0, 0, 0, 0.1);
+		font-size: 1.5em;
+		text-align: center;
+		margin: 10px 0;
+		padding: 5px 0;
+	}
+	ol,
+	ul {
+		height: 100%;
+		padding: 0;
+		list-style-position: inside;
+	}
+	li {
+		padding: 5px 16px;
+		border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+	}
+	.input {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 10px;
+	}
+	.clear-button {
+		margin: auto;
+	}
+	.priority {
+		width: 40px;
 	}
 </style>
