bf948bc482166d6a0b25f279606fe9144d8368f7
lingon <48642331+lingonsylt@users.noreply.github.com>
2025-12-15 15:10:24 +0100

    memory logic done TODO images, styling, start btn

--- src/routes/memory/+page.svelte
+++ src/routes/memory/+page.svelte
@@ -1,34 +1,82 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+	let cardBackURL =
+		'https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.bKAWEVqJT2nep1zID5WtkgHaKX%3Fpid%3DApi&sp=1765802805T944a6c6f2d94512778e11dc415edcdd0b0244f235c4d7e93c3a997443de7e815';
+	let cardPairs = [favicon];
 	const card_count = 12;
-	let cards = [];
+	let cards = $state([]);
 	for (let i = 0; i < card_count; i++) {
 		cards.push({
+			id: Math.floor(i / 2),
 			image: favicon,
 			flipped: false,
 			matched: false
 		});
 	}
+	let points = $state({
+		blue: 0,
+		red: 0
+	});
+	let turn = $state('blue');
 
-	let bluePoints = $state(0);
-	let redPoints = $state(0);
-	let turn = 'blue';
+	let cooldownActive = false;
+	function getFlipped() {
+		return cards.filter((c) => !c.matched && c.flipped);
+	}
+
+	function flip(i) {
+		if (cooldownActive || cards[i].flipped || getFlipped().length > 2) {
+			return;
+		}
+		cards[i].flipped = !cards[i].flipped;
+		if (getFlipped().length != 2) {
+			return;
+		}
+		let flippedCards = getFlipped();
+		if (flippedCards[0].id == flippedCards[1].id) {
+			flippedCards.forEach((c) => {
+				c.matched = true;
+			});
+			points[turn]++;
+		}
+		cooldownActive = true;
+		setTimeout(() => {
+			cards.forEach((c) => {
+				c.flipped = c.matched;
+			});
+			cooldownActive = false;
+			turn = turn == 'blue' ? 'red' : 'blue';
+		}, 1000);
+	}
 </script>
 
 <h1>Memory</h1>
 <main>
 	{#each cards as card, i (card)}
-		<div class="card">
-			<img src={card.image} alt="Kort nr {i}" class="card-img" />
-			<img src={card.image} alt="Baksida nr {i}" class="card-back" />
+		<div
+			class="card"
+			role="button"
+			tabindex="0"
+			class:flipped={card.flipped}
+			onclick={() => {
+				flip(i);
+			}}
+			onkeypress={(e) => {
+				if (e.key == 'Enter') {
+					flip(i);
+				}
+			}}
+		>
+			<img src={cardBackURL} alt="Baksida nr {i}" class="card-back" />
+			<img src={card.image} alt="Kort nr {i}" class="card-front" />
 		</div>
 	{/each}
 </main>
 <aside class="blue-score">
-	<p>{bluePoints}</p>
+	<p>{points.blue}</p>
 </aside>
 <aside class="red-score">
-	<p>{redPoints}</p>
+	<p>{points.red}</p>
 </aside>
 <aside class="turn-indicator {turn}-score"></aside>
 
@@ -40,11 +88,20 @@
 	.card {
 		position: relative;
 		border: 1px solid black;
+		transition: transform 0.5s;
+		transform-style: preserve-3d;
 	}
-	img {
+	.card img {
 		position: absolute;
 		width: 100%;
 		height: 100%;
+		backface-visibility: hidden;
+	}
+	.card-front {
+		transform: rotateY(180deg);
+	}
+	.flipped {
+		transform: rotateY(180deg);
 	}
 
 	aside {
