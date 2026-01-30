96210bfe1afae2ee57dec5d3b95c30c89af1bfa8
lingon <48642331+lingonsylt@users.noreply.github.com>
2026-01-30 22:47:24 +0100

    lite results image display

--- src/routes/search/[pokemon]/+page.svelte
+++ src/routes/search/[pokemon]/+page.svelte
@@ -3,31 +3,52 @@
 
 	const { params, data } = $props();
 
-	let pokemon = data.response;
+	let pokemon = $derived(data.response);
 
 	let sprites = $derived(
-		Object.entries(pokemon.sprites).map((sprite) => {
-			typeof sprite[1] == 'string' ? { img: sprite[1], name: sprite[0] } : null;
-		})
+		Object.entries(pokemon.sprites)
+			.map((sprite) => (typeof sprite[1] == 'string' ? { img: sprite[1], name: sprite[0] } : null))
+			.filter((x) => x != null)
 	);
+	let currentSpriteIndex = $state(0);
+	$inspect(pokemon);
 </script>
 
 {#if pokemon}
 	{(globalState.titleText = params.pokemon ? '' : '')}
 	<h1>{pokemon.name}</h1>
 	<article>
-		{#each sprites as sprite (sprite)}
-			<img src={sprite.img} alt="Sprite [{sprite.name}]" />
-		{/each}
+		<div class="info-container">
+			<h2>{pokemon.name}</h2>
+			<p>ID: {pokemon.id}</p>
+			<p>Weight: {pokemon.weight}kg?</p>
+		</div>
+		<div class="image-display">
+			{#each sprites as sprite, i (sprite)}
+				<img src={sprite.img} alt="Sprite [{sprite.name}]" />
+			{/each}
+		</div>
 	</article>
-	<div class="image-display"></div>
+{:else}
+	<p>Search for a pokemon to see its information here!</p>
 {/if}
 
 <style>
 	article {
-		display: flex;
-		flex-wrap: wrap;
 		width: 90%;
-		gap: 1em;
+		height: 100%;
+		display: flex;
+		justify-content: space-between;
+	}
+	.image-display {
+		position: relative;
+		width: 50%;
+	}
+	.image-display img {
+		position: absolute;
+		width: 200px;
+		background-color: gainsboro;
+		border-radius: 10px;
+		border: 2px solid black;
 	}
 </style>
